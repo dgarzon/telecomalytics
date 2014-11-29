da73f89affee47c6e7e0924d12d83663bc8cb9c2
Daniel Garzon <dg2796@columbia.edu>
2014-11-29 14:33:46 -0500

    Loop through directories done.

--- Calculator.py
+++ Calculator.py
@@ -15,11 +15,16 @@ def prompt():
     pass
 
 
+def get_fcc_entry(path):
+    print path
+    # book = xlrd.open_workbook(path)
+
+
 def parse_fcc_data():
     for subdir, dirs, files in os.walk(ROOT_DIR + "/FCC"):
         for file in files:
             if file.endswith(".xls"):
-                print file
+                get_fcc_entry(os.path.join(subdir, file))
     pass
 
 
@@ -27,7 +32,7 @@ def parse_world_bank_data():
     for subdir, dirs, files in os.walk(ROOT_DIR + "/WB"):
         for file in files:
             if file.endswith(".xls"):
-                print file
+                print os.path.join(subdir, file)
     pass
 
 
