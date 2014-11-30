497af1f09d297909bda8fc388a5c15a995272803
Daniel Garzon <dg2796@columbia.edu>
2014-11-29 21:25:07 -0500

    Parsing the GDP is now possible.

--- Calculator.py
+++ Calculator.py
@@ -3,11 +3,12 @@ import os
 import time
 import xlrd
 from FCCEntry import FCCEntry
+from WBEntry import WBEntry
 
 
 ROOT_DIR = "."
 FCC = {}
-WB = {}
+WB = []
 
 
 class TrafficBilledInTheUSA(object):
@@ -52,6 +53,14 @@ class TotalUSCarriers(object):
     def __init__(self):
         super(TotalUSCarriers, self).__init__()
         self.retained_revenues = 0
+
+
+class WorldBankData(object):
+    """docstring for WorldBankData"""
+    def __init__(self):
+        super(WorldBankData, self).__init__()
+        self.year = 0
+        self.gdp = 0
 
 
 def prompt():
@@ -179,11 +188,134 @@ def parse_fcc_data():
     pass
 
 
+def get_world_bank_entry(path):
+    # print path
+    entries = []
+
+    workbook = xlrd.open_workbook(path)
+    worksheet = workbook.sheet_by_name(workbook.sheet_names()[0])
+
+    num_rows = worksheet.nrows - 1
+    num_cells = worksheet.ncols - 1
+    curr_row = -1
+    while curr_row < num_rows:
+        curr_cell = -1
+        curr_row += 1
+
+        row = worksheet.row(curr_row)
+
+        entry = WBEntry()
+
+        while curr_cell < num_cells:
+            curr_cell += 1
+            cell_type = worksheet.cell_type(curr_row, curr_cell)
+            cell_value = worksheet.cell_value(curr_row, curr_cell)
+
+            if curr_cell == 0:
+                entry.country_name = str(cell_value)
+            else:
+                data = WorldBankData()
+
+                if curr_cell == 1:
+                    data.year = 1992
+                    data.gdp = float(cell_value)
+                    pass
+                elif curr_cell == 2:
+                    data.year = 1993
+                    data.gdp = float(cell_value)
+                    pass
+                elif curr_cell == 3:
+                    data.year = 1994
+                    data.gdp = float(cell_value)
+                    pass
+                elif curr_cell == 4:
+                    data.year = 1995
+                    data.gdp = float(cell_value)
+                    pass
+                elif curr_cell == 5:
+                    data.year = 1996
+                    data.gdp = float(cell_value)
+                    pass
+                elif curr_cell == 6:
+                    data.year = 1997
+                    data.gdp = float(cell_value)
+                    pass
+                elif curr_cell == 7:
+                    data.year = 1998
+                    data.gdp = float(cell_value)
+                    pass
+                elif curr_cell == 8:
+                    data.year = 1999
+                    data.gdp = float(cell_value)
+                    pass
+                elif curr_cell == 9:
+                    data.year = 2000
+                    data.gdp = float(cell_value)
+                    pass
+                elif curr_cell == 10:
+                    data.year = 2001
+                    data.gdp = float(cell_value)
+                    pass
+                elif curr_cell == 11:
+                    data.year = 2002
+                    data.gdp = float(cell_value)
+                    pass
+                elif curr_cell == 12:
+                    data.year = 2003
+                    data.gdp = float(cell_value)
+                    pass
+                elif curr_cell == 13:
+                    data.year = 2004
+                    data.gdp = float(cell_value)
+                    pass
+                elif curr_cell == 14:
+                    data.year = 2005
+                    data.gdp = float(cell_value)
+                    pass
+                elif curr_cell == 15:
+                    data.year = 2006
+                    data.gdp = float(cell_value)
+                    pass
+                elif curr_cell == 16:
+                    data.year = 2007
+                    data.gdp = float(cell_value)
+                    pass
+                elif curr_cell == 17:
+                    data.year = 2008
+                    data.gdp = float(cell_value)
+                    pass
+                elif curr_cell == 18:
+                    data.year = 2009
+                    data.gdp = float(cell_value)
+                    pass
+                elif curr_cell == 19:
+                    data.year = 2010
+                    data.gdp = float(cell_value)
+                    pass
+                elif curr_cell == 20:
+                    data.year = 2011
+                    data.gdp = float(cell_value)
+                    pass
+                elif curr_cell == 21:
+                    data.year = 2012
+                    data.gdp = float(cell_value)
+                    pass
+                else:
+                    print "Error: Cell index out of supported range."
+
+                entry.data.append(data.__dict__)
+        entries.append(entry)
+
+    return entries
+
+
 def parse_world_bank_data():
+    global WB
     for subdir, dirs, files in os.walk(ROOT_DIR + "/WB"):
         for file in files:
             if file.endswith(".xls"):
-                print os.path.join(subdir, file)
+                entries = get_world_bank_entry(os.path.join(subdir, file))
+                WB = entries
     pass
 
 
@@ -195,11 +327,17 @@ def print_fcc_data_structure():
     pass
 
 
+def print_world_bank_data_structure():
+    for item in WB:
+        print item
+
+
 def main():
     prompt()
-    parse_fcc_data()
+    # parse_fcc_data()
     # print_fcc_data_structure()
-    # parse_world_bank_data()
+    parse_world_bank_data()
+    print_world_bank_data_structure()
 
 
 if __name__ == "__main__":
--- WBEntry.py
+++ WBEntry.py
@@ -2,3 +2,15 @@ class WBEntry(object):
     """docstring for WBEntry"""
     def __init__(self):
         super(WBEntry, self).__init__()
+        self.country_name = ""
+        self.data = []
+
+    def __str__(self):
+        obj = ""
+        obj += "\tcountry_name: " + self.country_name + "\n"
+        obj += "\tdata: \n"
+        for item in self.data:
+            obj += "\t\t" + "year: " + str(item["year"]) + "\n"
+            obj += "\t\t" + "gdp: " + str(item["gdp"]) + "\n"
+
+        return obj
